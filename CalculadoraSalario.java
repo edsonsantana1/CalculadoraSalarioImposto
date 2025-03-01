3b261eb8e522e162d49202f626ed7156b664e281
Edson Santana Alves <edson_santana08@outlook.com>
2025-03-01 17:26:39 -0300

    Adiciona CalculadoraSalario.java

---
+++ CalculadoraSalario.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class CalculadoraSalario {
+    public static void main(String[] args) {
+        Scanner leitorDeEntrada = new Scanner(System.in);
+        float valorSalario = leitorDeEntrada.nextFloat();
+        float valorBeneficios = leitorDeEntrada.nextFloat();
+
+        float valorImposto = 0;
+        if (valorSalario >= 0 && valorSalario <= 1100) {
+            valorImposto = 0.05F * valorSalario;
+        }
+        else if (valorSalario >= 1100.01 && valorSalario <= 2500.00) {
+            valorImposto += 0.10F * valorSalario;
+        }
+        else {
+            valorImposto += 0.15F * valorSalario;
+        }
+        
+        float saida = valorSalario - valorImposto + valorBeneficios;
+        System.out.println(String.format("%.2f", saida));
+    }
+}
